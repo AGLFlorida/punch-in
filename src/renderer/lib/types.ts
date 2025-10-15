889ba3c87f759a9aca299330da8a84d564bdfc0d
Brandon Shoop <brandon.shoop@gmail.com>
2025-10-15 11:35:38 -0400

    chore(ci): add clean commands, fix type error

--- src/renderer/lib/types.ts
+++ src/renderer/lib/types.ts
@@ -1,3 +1,10 @@
 export type Session = { project: string; start: number; end: number };
 
+export type State = {
+  sessions: Session[];
+  isRunning: boolean;
+  currentProject: string | null;
+  currentStart: number | null;
+};
+
 export type TimerCallback = () => void;
