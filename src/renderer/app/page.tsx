b901ff54432079ee787aa5f30e63c5350352ffc9
Brandon Shoop <3355821+1shooperman@users.noreply.github.com>
2025-10-17 11:45:45 -0400

    fix: react hydration issue in UI build. (#10)

--- src/renderer/app/page.tsx
+++ src/renderer/app/page.tsx
@@ -1,14 +1,12 @@
-import Sidebar from '@/components/Sidebar';
-
 export default function Home() {
   return (
-    <Sidebar>
+    <>
       <div className="header">
         <h1 className="title">Timer</h1>
       </div>
       <div className="content">
         <p>Welcome to PunchIn. Please select an option from the menu on the left.</p>
       </div>
-    </Sidebar>
+    </>
   );
 }
